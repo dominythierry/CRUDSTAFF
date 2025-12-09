a8db1876ced00b1d69913ed29dcfe72d0c30df2c
Dominy Thierry Anjos Ramos <dominy.thierry2007@outlook.com>
2025-12-08 21:49:01 -0300

    Switch to Neon database

--- back-tarefas/services/db.js
+++ back-tarefas/services/db.js
@@ -1,13 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "vehicle_info",
-  port: process.env.DB_PORT || 5432,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-  family: 4 // ⭐ Força IPv4
+  connectionString: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_tXy9sdcTu5ig@ep-round-moon-acer9avg-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require",
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.connect((err, client, release) => {
